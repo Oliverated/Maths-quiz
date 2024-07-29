3e0277f16398e16c60a4d11c0ae31f08b1f3809a
Ovbiebo Oliver <victorlodoliver@gmail.com>
2024-07-29 23:28:10 +0100

    fixed and add some js function

--- math-quiz.js
+++ math-quiz.js
@@ -15,11 +15,8 @@ let atp_note = document.querySelector(".attemps");
 let checkBtn = document.querySelector(".check");
 let genBtn = document.querySelector(".generate");
 let attempts = 0;
-
-let add = ''
-let sub = ""
-let mul =""
-let div =""
+let attempts2 = 0;
+let answer;
 
 // random number generation
 function gen_ran_func() {
@@ -27,26 +24,25 @@ function gen_ran_func() {
   let ran2 = Math.floor(Math.random() * 10);
   let ran_operator = Math.floor(Math.random() * 4);
 
-  console.log(ran_operator);
-
   numb1.innerHTML = ran1;
   numb2.innerHTML = ran2;
 
-  if (ran_operator === 0) {
-    add = ran1 + ran2;
-    operator_sign.innerHTML = "+";
-  } else if (ran_operator === 1) {
-    sub = ran1 - ran2;
-    operator_sign.innerHTML = "-";
-  } else if (ran_operator === 2) {
-    div = ran1 / ran2;
-    operator_sign.innerHTML = "/";
-  } else if (ran_operator === 3) {
-    mul = ran1 * ran2;
-    operator_sign.innerHTML = "x";
+  function random_operator() {
+    if (ran_operator === 0) {
+      answer = ran1 + ran2;
+      operator_sign.innerHTML = "+";
+    } else if (ran_operator === 1) {
+      answer = ran1 - ran2;
+      operator_sign.innerHTML = "-";
+    } else if (ran_operator === 2) {
+      answer = ran1 / ran2;
+      operator_sign.innerHTML = "/";
+    } else if (ran_operator === 3) {
+      answer = ran1 * ran2;
+      operator_sign.innerHTML = "x";
+    }
   }
-console.log(add)
-  console.log(operator_sign);
+  random_operator();
 }
 
 genBtn.addEventListener("click", () => {
@@ -62,84 +58,54 @@ continueBtn.addEventListener("click", () => {
     block1.style.display = "none";
     name_value.textContent = name_inp.value;
   }
-
   gen_ran_func();
 });
 
 // answer input validation
 ansBtn.addEventListener("click", () => {
-  // try {
+  try {
     if (ans_inp.value == "") {
       ans_note.innerHTML = "Empty Input";
       ans_note.style.color = "yellow";
-    } else if (ans_inp.value == add  ) {
-      ans_note.innerHTML = "Correct!!!";
-      ans_note.style.color = "green";
-      main_block.style.borderLeft = "5px solid green";
-      console.log(attempts);
-      atp_note.innerHTML = "You made " + attempts + " attemps";
-      attempts = 0;
-      // block2.style.background = 'orange'
-    } else if (ans_inp.value ==  sub  ) {
-      ans_note.innerHTML = "Correct!!!";
-      ans_note.style.color = "green";
-      main_block.style.borderLeft = "5px solid green";
-      console.log(attempts);
-      atp_note.innerHTML = "You made " + attempts + " attemps";
-      attempts = 0;
-      // block2.style.background = 'orange'
-    } else if (ans_inp.value ==  mul  ) {
-      ans_note.innerHTML = "Correct!!!";
-      ans_note.style.color = "green";
-      main_block.style.borderLeft = "5px solid green";
-      console.log(attempts);
-      atp_note.innerHTML = "You made " + attempts + " attemps";
-      attempts = 0;
-      // block2.style.background = 'orange'
-    }  else if (ans_inp.value == div ) {
-      ans_note.innerHTML = "Correct!!!";
-      ans_note.style.color = "green";
-      main_block.style.borderLeft = "5px solid green";
-      console.log(attempts);
-      atp_note.innerHTML = "You made " + attempts + " attemps";
-      attempts = 0;
-      // block2.style.background = 'orange'
-    }
-     else if (isNaN(ans_inp.value)) {
+      main_block.style.borderLeft = "5px solid yellow";
+    } else if (isNaN(ans_inp.value)) {
       ans_note.innerHTML = "numbers only";
-      ans_note.style.color = "yellow";
-    } else if (ans_inp.value != add ) {
-      ans_note.innerHTML = "wrong answer! try again!!";
-      ans_note.style.color = "red";
-      attempts++;
-      console.log(attempts);
-      function atp_func() {
-        if (attempts === 3) {
-          ans_note.innerHTML = "are you sure you want to continue?";
-        } else if (attempts === 5) {
-          ans_note.innerHTML = "enter generate to a different number";
-        } else if (attempts === 6) {
-          gen_ran_func();
-        }
+      ans_note.style.color = "darkyellow";
+      main_block.style.borderLeft = "5px solid yellow";
+    } else if (ans_inp.value == answer) {
+      ans_note.innerHTML = name_inp.value + " you won! it Correct!!!";
+      ans_note.style.color = "green";
+      main_block.style.borderLeft = "5px solid green";
+      atp_note.innerHTML = name_inp.value + " You made " + attempts + " attemps";
+      attempts = 0;
+      ansBtn.textContent = "Continue";
+      ans_inp.value.textContent = "you won " + name_inp.value;
+      if (ans_inp.disabled == true) {
+        ansBtn.textContent = "Answer";
+        ans_inp.disabled = false;
+        gen_ran_func();
+      } else {
+        ans_inp.disabled = true;
       }
-
-      atp_func();
+    } else if (ans_inp.value != answer) {
+      ans_note.innerHTML = name_inp.value + " wrong answer! try again!!";
+      ans_note.style.color = "red";
+      main_block.style.borderLeft = "5px solid red";
+      attempts++;
+      attempts2++;
+      atp_note.innerHTML = "";
+      ans_inp.disabled = false;
+      if (attempts2 === 3) {
+        ans_note.innerHTML = "are you sure you want to continue?";
+      } else if (attempts2 === 5) {
+        ans_note.innerHTML = "if wrong it generate a new number";
+      } else if (attempts2 === 6) {
+        gen_ran_func();
+        attempts2 = 0;
+      }
     }
-// gen_ran_func()
-  // }
-  //  catch (error) {
-  //   ans_note.textContent = "unespected error!";
-  //   ans_note.style.color = "red";
-  // }
+  } catch (error) {
+    ans_note.textContent = "unespected error!";
+    ans_note.style.color = "red";
+  }
 });
-
-
-
-if (typeof(add) == 'string'){
-console.log('good')
-}
-
-
-let b = 2 + 4 
-
-console.log(b)
